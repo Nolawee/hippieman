47f29a21bcc4629d1c28f1e23627441211e17510
Nolawee Mengist <nome9478@colorado.edu>
2017-02-27 11:23:03 -0700

    changed image and background color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,62 +1,62 @@
 $(document).ready(function() {
-	
+
 	/*============================================
 	Page Preloader
 	==============================================*/
-	
+
 	$(window).load(function(){
 		$('#page-loader').fadeOut(500,function(){
 			loadGmap();
 		});
-		
-	})	
-	
+
+	})
+
 	/*============================================
 	Header
 	==============================================*/
-	
+
 	$('#home').height($(window).height()+50);
-	
-	$.backstretch('assets/images/header-bg.jpg');
-	
+
+	$.backstretch('assets/images/straight.jpg');
+
 	if($(window).scrollTop() > ($(window).height()+50)){
 		$('.backstretch').hide();
 	}else{
 		$('.backstretch').show();
 	}
-	
+
 	$(window).scroll( function() {
 		var st = $(this).scrollTop(),
 			wh = $(window).height(),
 			sf = 1.2 - st/(10*wh);
-		
-		$('.backstretch img').css({ 
-			'transform' : 'scale('+sf+')', 
+
+		$('.backstretch img').css({
+			'transform' : 'scale('+sf+')',
 			'-webkit-transform' : 'scale('+sf+')'
 		});
-		
+
 		$('#home .container').css({ 'opacity' : (1.4 - st/400) });
-		
+
 		if($(window).scrollTop() > ($(window).height()+50)){
 			$('.backstretch').hide();
 		}else{
 			$('.backstretch').show();
 		}
-		
+
 	});
-	
+
 	var st = $(this).scrollTop(),
 		wh = $(window).height(),
 		sf = 1.2 - st/(10*wh);
 
-	$('.backstretch img').css({ 
-		'transform' : 'scale('+sf+')', 
+	$('.backstretch img').css({
+		'transform' : 'scale('+sf+')',
 		'-webkit-transform' : 'scale('+sf+')'
 	});
 
 	$('#home .container').css({ 'opacity' : (1.4 - st/400) });
 
-	
+
 	/*============================================
 	Navigation Functions
 	==============================================*/
@@ -64,7 +64,7 @@ $(document).ready(function() {
 		$('#main-nav').removeClass('scrolled');
 	}
 	else{
-		$('#main-nav').addClass('scrolled');    
+		$('#main-nav').addClass('scrolled');
 	}
 
 	$(window).scroll(function(){
@@ -72,10 +72,10 @@ $(document).ready(function() {
 			$('#main-nav').removeClass('scrolled');
 		}
 		else{
-			$('#main-nav').addClass('scrolled');    
+			$('#main-nav').addClass('scrolled');
 		}
 	});
-	
+
 	/*============================================
 	ScrollTo Links
 	==============================================*/
@@ -96,13 +96,13 @@ $(document).ready(function() {
 
 		$(this).data('height',perc);
 	})
-	
+
 	$('.touch .shows-item').each(function(){
 		$(this).css({'height':$(this).data('height')+'%'});
 	})
-	
+
 	$('.touch .shows-bars').css({'opacity':1});
-		
+
 	/*============================================
 	Project thumbs - Masonry
 	==============================================*/
@@ -121,19 +121,19 @@ $(document).ready(function() {
 
 		scrollSpyRefresh();
 		waypointsRefresh();
-		
+
 	});
-	
+
 	/*============================================
 	Filter Projects
 	==============================================*/
 	$('#filter-works a').click(function(e){
 		e.preventDefault();
-		
+
 		if($('#project-preview').hasClass('open')){
 			closeProject();
 		}
-		
+
 		$('#filter-works li').removeClass('active');
 		$(this).parent('li').addClass('active');
 
@@ -153,7 +153,7 @@ $(document).ready(function() {
 		scrollSpyRefresh();
 		waypointsRefresh();
 	});
-	
+
 	/*============================================
 	Project Preview
 	==============================================*/
@@ -171,26 +171,26 @@ $(document).ready(function() {
 		for (var i = 0; i < slides.length; ++i) {
 			slidesHtml = slidesHtml + '<li><img src='+slides[i]+' alt=""></li>';
 		}
-		
+
 		slidesHtml = slidesHtml + '</ul>';
-		
+
 		$('#project-title').text(title);
 		$('#project-content').html(descr);
 		$('#project-slider').html(slidesHtml);
-		
+
 		openProject();
-		
+
 	});
 
 	function openProject(){
-		
+
 		$('#project-preview').addClass('open');
 		$('.masonry-wrapper').animate({'opacity':0},300);
-		
+
 		setTimeout(function(){
 			$('#project-preview').slideDown();
 			$('.masonry-wrapper').slideUp();
-			
+
 			$('html,body').scrollTo(0,'#filter-works',
 				{
 					gap:{y:-20},
@@ -198,15 +198,15 @@ $(document).ready(function() {
 						duration:400
 					}
 			});
-			
+
 			$('#project-slider').flexslider({
 				prevText: '<i class="fa fa-angle-left"></i>',
 				nextText: '<i class="fa fa-angle-right"></i>',
 				animation: 'slide',
 				slideshowSpeed: 3000,
 				useCSS: true,
-				controlNav: true, 
-				pauseOnAction: false, 
+				controlNav: true,
+				pauseOnAction: false,
 				pauseOnHover: true,
 				smoothHeight: false,
 				start: function(){
@@ -214,41 +214,41 @@ $(document).ready(function() {
 					$('#project-preview').animate({'opacity':1},300);
 				}
 			});
-			
+
 		},300);
-		
+
 	}
-	
+
 	function closeProject(){
-	
+
 		$('#project-preview').removeClass('open');
 		$('#project-preview').animate({'opacity':0},300);
-		
+
 		setTimeout(function(){
 			$('.masonry-wrapper').slideDown();
 			$('#project-preview').slideUp();
-				
+
 			$('#project-slider').flexslider('destroy');
 			$('.masonry-wrapper').animate({'opacity':1},300);
-			
-			
+
+
 		},300);
-		
+
 		setTimeout(function(){
 			$('#projects-container').masonry('reload');
 		},500)
 	}
-	
+
 	$('.close-preview').click(function(){
 		closeProject();
 	})
-	
+
 	/*============================================
 	Twitter
 	==============================================*/
 	var tweetsLength = $('#twitter-slider').data('tweets-length'),
 		widgetID = $('#twitter-slider').data('widget-id');
-	
+
 	var configTweets = {
 	  "id": widgetID,
 	  "domId": '',
@@ -261,34 +261,34 @@ $(document).ready(function() {
 	  "customCallback": handleTweets,
 	  "showInteraction": true
 	};
-	
+
 	twitterFetcher.fetch(configTweets);
 
 	function handleTweets(tweets){
-	
+
 		var x = tweets.length,
 			n = 0,
 			tweetsHtml = '<ul class="slides">';
-			
+
 		while(n < x) {
 			tweetsHtml += '<li>' + tweets[n] + '</li>';
 			n++;
 		}
-		
+
 		tweetsHtml += '</ul>';
 		$('#twitter-slider').html(tweetsHtml);
-	
+
 		$('.twitter_reply_icon').html("<i class='fa fa-reply'></i>");
 		$('.twitter_retweet_icon').html("<i class='fa fa-retweet'></i>");
 		$('.twitter_fav_icon').html("<i class='fa fa-heart'></i>");
-	  
+
 		$('#twitter-slider').flexslider({
 			prevText: '<i class="fa fa-angle-left"></i>',
 			nextText: '<i class="fa fa-angle-right"></i>',
 			slideshowSpeed: 5000,
 			useCSS: true,
-			controlNav: false, 
-			pauseOnAction: false, 
+			controlNav: false,
+			pauseOnAction: false,
 			pauseOnHover: true,
 			smoothHeight: false
 		});
@@ -297,14 +297,14 @@ $(document).ready(function() {
 	Contact Map
 	==============================================*/
 	function loadGmap(){
-	
+
 	if($('#gmap').length){
-	
+
 		var map;
 		var mapstyles = [ { "stylers": [ { "saturation": -100 } ] } ];
-		
+
 		var infoWindow = new google.maps.InfoWindow;
-		
+
 		var pointLatLng = new google.maps.LatLng(mapPoint.lat, mapPoint.lng);
 
 		var mapOptions = {
@@ -318,18 +318,18 @@ $(document).ready(function() {
 			scrollwheel: false,
 			styles: mapstyles
 		}
-		
+
 		map = new google.maps.Map(document.getElementById("gmap"), mapOptions);
-		
+
 		var marker = new google.maps.Marker({
-			position: pointLatLng, 
-			map: map, 
+			position: pointLatLng,
+			map: map,
 			title:mapPoint.linkText,
 			icon: mapPoint.icon
 		});
-		
+
 		var mapLink = 'https://www.google.com/maps/preview?ll='+mapPoint.lat+','+mapPoint.lng+'&z=14&q='+mapPoint.mapAddress;
-		
+
 		var html = '<div class="infowin">'
 				+ mapPoint.infoText
 				+ '<a href="'+mapLink+'" target="_blank">'+mapPoint.linkText+'</a>'
@@ -343,44 +343,44 @@ $(document).ready(function() {
 		google.maps.event.addListener(marker, 'click', function() {
 			window.open(mapLink,'_blank');
 		});
-		
+
 	}
 	}
 	/*============================================
 	Waypoints Animations
 	==============================================*/
 	$('#shows').waypoint(function(){
-	
+
 		$('.shows-item').each(function(){
 			$(this).css({'height':$(this).data('height')+'%'});
 		})
-		
+
 		$('.shows-bars').css({'opacity':1});
-		
+
 	},{offset:'40%'});
-	
+
 	$('.scrollimation').waypoint(function(){
 		$(this).addClass('in');
 	},{offset:'90%'});
-	
+
 	/*============================================
 	Resize Functions
 	==============================================*/
 	var thumbSize = $('.project-item').width();
-	
+
 	$(window).resize(function(){
 		$('#home').height($(window).height()+50);
-		
+
 		if($('.project-item').width() != thumbSize){
-		
+
 			$('#projects-container').masonry('reload');
 			thumbSize = $('.project-item').width();
 		}
-		
+
 		scrollSpyRefresh();
 		waypointsRefresh();
 	});
-	
+
 	/*============================================
 	Refresh scrollSpy function
 	==============================================*/
@@ -389,7 +389,7 @@ $(document).ready(function() {
 			$('body').scrollspy('refresh');
 		},1000);
 	}
-	
+
 	/*============================================
 	Refresh waypoints function
 	==============================================*/
